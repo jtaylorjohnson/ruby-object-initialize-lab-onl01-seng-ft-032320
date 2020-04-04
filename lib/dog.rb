db3252ccc0675d8472edb2dd2a32cc12d538da61
James Johnson <jtaylorjohnson@gmail.com>
2020-04-04 23:30:08 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog 
+  def initilize(name, breed)
+    @name = name
+    if breed != null
+      @breed = breed
+    else @breed = "Mutt"
+  end
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+  attr_accessor :name 
+  def initilize(name)
+    @name = name
+  end
+  
+end

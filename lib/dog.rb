10f848b0e89efd7be56dc4a1c19bc9889259dd09
James Johnson <jtaylorjohnson@gmail.com>
2020-04-04 23:42:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,8 @@ class Dog
   def initialize(name, breed)
     @name = name
     if breed = ""
-      @breed = breed
-    else @breed = "Mutt"
+      @breed = "Mutt"
+    else @breed = breed
   end
   end
   attr_accessor :name 
